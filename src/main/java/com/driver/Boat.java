1dd96b360aef6d9612fbfe4aa85643d35fc03853
Bhanu-Laila <bhanulaila1998@gmail.com>
2023-08-15 03:00:07 +0530

    changed code

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,36 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,11 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4,4,6,isManual, "luxury", 4)
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate;; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,6 +21,20 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setGears(1);
+        } else if (newSpeed>=1 && newSpeed<=50) {
+            setGears(1);
+        } else if (newSpeed>=51 && newSpeed<=100) {
+            setGears(2);
+        } else if (newSpeed>=101 && newSpeed<=150) {
+            setGears(3);
+        } else if (newSpeed>=151 && newSpeed<=200) {
+            setGears(4);
+        } else if (newSpeed>=201 && newSpeed<=250) {
+            setGears(5);
+        }else{
+            setGears(6);
         }
         //for all other cases, change the gear accordingly
 
